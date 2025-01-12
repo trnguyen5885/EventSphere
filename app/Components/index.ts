8daab3e0f890f1f50d46ef4618b279917c9a0689
Nguyễn Nhật Nam <nhatnam5122004@gmail.com>
2025-01-12 19:26:09 +0700

    add SpaceComponent, RowComponent, SectionComponent, and InputComponent to index export

--- app/Components/index.ts
+++ app/Components/index.ts
@@ -1,7 +1,15 @@
 import ButtonComponent from './ButtonComponent';
+import SpaceComponent from './SpaceComponent';
 import TextComponent from './TextComponent';
+import RowComponent from './RowComponent';
+import SectionComponent from './SectionComponent';
+import InputComponent from './InputComponent';
 
 export {
     ButtonComponent,
     TextComponent,
+    SpaceComponent,
+    RowComponent,
+    SectionComponent,
+    InputComponent
 }
