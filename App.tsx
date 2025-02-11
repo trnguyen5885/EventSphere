148a17cf889c6ab06eecb51e20afc193b625b30c
Nguyễn Trần Trung Nguyên <macos@nguyentrantrungnguyenk4it.local>
2025-02-11 17:17:16 +0700

    develop

--- App.tsx
+++ App.tsx
@@ -4,7 +4,12 @@ import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { WelcomeScreen, LoginScreen, RegisterScreen } from "./app/screens";
+import {
+  WelcomeScreen,
+  LoginScreen,
+  RegisterScreen,
+  HomeScreen,
+} from "./app/screens";
 
 const Stack = createStackNavigator();
 
@@ -39,6 +44,7 @@ const App = () => {
           <Stack.Screen name="Welcome" component={WelcomeScreen} />
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Register" component={RegisterScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </GestureHandlerRootView>
--- app/screens/home/HomeScreen.tsx
+++ app/screens/home/HomeScreen.tsx
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const HomeScreen = () => {
   return (
     <View>
       <Text>HomeScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
