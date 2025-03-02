2d161472eabf50a3643cb40cb0dd50ca0d344306
Nguyễn Trần Trung Nguyên <macos@nguyentrantrungnguyenk4it.local>
2025-03-02 19:58:28 +0700

    Finish UI Category and Search

---
+++ app/screens/explore/EventSearchScreen.jsx
@@ -0,0 +1,90 @@
+import { StyleSheet, Text, View, Platform,StatusBar,FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Ionicons } from '@expo/vector-icons';
+import { InputComponent, RowComponent } from '@/app/components';
+import EventItem from '@/app/components/EventItem';
+import { globalStyles } from '@/app/constants/globalStyles'
+import { appColors } from '@/app/constants/appColors';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { AxiosInstance } from '@/app/services';
+
+
+
+const EventSearch = ({navigation}) => {
+
+  const [values, setValues] = useState("");
+  const [eventsSearch, setEventsSearch] = useState([]);
+
+  const handleNavigation = () => {
+    navigation.goBack();
+  }
+
+  useEffect(() => {
+    
+    const getEventSearch = async () => {
+      try {
+        const response = await AxiosInstance().get("events/all");
+        setEventsSearch(response.data)
+      } catch(e) {
+        console.log(e)
+      }
+    }
+
+    getEventSearch();
+
+    return () => {
+      setEventsSearch([]);
+    }
+  },[])
+
+  return (
+    <View style={[globalStyles.container]} >
+      <View style={styles.header}>
+            <StatusBar animated backgroundColor={appColors.primary} />
+            <RowComponent onPress={handleNavigation} styles = {{columnGap: 25}}>
+                <Ionicons name="chevron-back" size={26} color="white" />
+            
+                <Text style = {styles.headerTitle} >Tìm kiếm</Text>
+            </RowComponent>
+
+              <InputComponent
+                value={values}
+                onChange={(text) => setValues(text)}
+                allowClear
+                customStyles={{minHeight: 46}}
+                affix={<MaterialIcons name="search" size={24} color="rgba(0,0,0,0.5)" />}
+            />
+      </View>
+
+      <FlatList
+          data={eventsSearch}
+          renderItem={({ item }) => <EventItem onPress={() => {
+            navigation.navigate("Detail", {
+              id: item._id
+            })
+          }} type="card" styles={{width: "95%"}} item={item} />}
+      />
+
+      
+        
+    </View>
+  )
+}
+
+export default EventSearch
+
+const styles = StyleSheet.create({
+  header:
+  {
+          alignItems: "flex-start",
+          padding: 12,
+          rowGap: 20,
+          backgroundColor: appColors.primary,
+          paddingTop: Platform.OS === "ios" ? 66 : 22
+  },
+  headerTitle: {
+    color: appColors.white2,
+    fontSize: 22,
+    fontWeight: "500"
+  },
+})
--- app/screens/index.ts
+++ app/screens/index.ts
@@ -6,8 +6,23 @@ import EventScreen from "./event/EventScreen"
 import MapScreen from "./map/MapScreen"
 import NotificationScreen from "./notification/NotificationScreen"
 import AddEventScreen from "./add/AddEventScreen"
-import EventDetailScreen from "./detail/EventDetailScreen"
+import EventCategoryScreen from "./explore/EventCategoryScreen"
+import EventDetailScreen from "./explore/EventDetailScreen"
+import EventSearchScreen from "./explore/EventSearchScreen"
 import TicketEventScreen from "./ticket/TicketEventScreen"
 
 
-export {WelcomeScreen, LoginScreen, RegisterScreen,ExploreScreen, EventScreen, MapScreen, NotificationScreen,AddEventScreen,EventDetailScreen, TicketEventScreen  }
+export {
+  WelcomeScreen,
+  LoginScreen,
+  RegisterScreen,
+  ExploreScreen,
+  EventScreen,
+  MapScreen,
+  NotificationScreen,
+  AddEventScreen,
+  EventDetailScreen,
+  TicketEventScreen,
+  EventCategoryScreen,
+  EventSearchScreen
+  }
